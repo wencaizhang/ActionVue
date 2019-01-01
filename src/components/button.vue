7f311effc23717f993b9869ccc2cfb69d9d81175
wencaizhang <1052642137@qq.com>
2019-01-01 12:08:46 +0800

    update

---
+++ src/components/button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>按钮</button>
+</template>
+
+<script>
+export default {
+  name: "g-button",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <gbutton>按钮</gbutton>
+  </div>
+</template>
+
+<script>
+import button from '@/components/button'
+
+export default {
+  components: {
+    gbutton: button,
+    
+  },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
