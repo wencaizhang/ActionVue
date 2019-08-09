6e4fb58cb6bd0cbb5a409a6e9faaf3958a8fcb68
wencaizhang <1052642137@qq.com>
2019-08-09 19:27:52 +0800

    docs: create docs about swipe

---
+++ docs/.vuepress/components/swipe/demo1.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-swipe>
+    <a-swipe-item>content of swipe 1</a-swipe-item>
+    <a-swipe-item>content of swipe 2</a-swipe-item>
+  </a-swipe>
+</template>
+
+<style lang="scss" scoped>
+.a-swipe-item {
+  min-height: 160px;
+  background: #364d79;
+  color: #fff;
+}
+</style>
---
+++ docs/components/swipe.md
@@ -0,0 +1,13 @@
+# Swipe 轮播图
+
+<swipe-demo1 />
+
+
+
+## API
+
+| 属性   | 说明         | 类型    | 默认值 |
+| :----- | :----------- | :------ | :----- |
+| `loop` | 开启无限轮播 | Boolean | `true` |
+| `autoPlay` | 自动切换 | Boolean | `false` |
+| `delay` | 自动切换的时间间隔，单位 ms | Number | `3000` |
