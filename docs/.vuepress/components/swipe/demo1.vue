faccf5dd4faad3267f6c0df7db803de19936913f
wencaizhang <1052642137@qq.com>
2019-08-14 09:34:56 +0800

    docs: update docs for swipe

--- docs/.vuepress/components/swipe/demo1.vue
+++ docs/.vuepress/components/swipe/demo1.vue
@@ -1,14 +1,41 @@
 <template>
-  <a-swipe>
-    <a-swipe-item>content of swipe 1</a-swipe-item>
-    <a-swipe-item>content of swipe 2</a-swipe-item>
+  <a-swipe :seleted.async="selected" @change="onChange">
+    <a-swipe-item class="blue" name="1a">content of swipe 1</a-swipe-item>
+    <a-swipe-item class="red" name="2b">content of swipe 2</a-swipe-item>
+    <a-swipe-item class="green" name="3c">content of swipe 3</a-swipe-item>
   </a-swipe>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      selected: '1a',
+    }
+  },
+  methods: {
+    onChange(a, b) {
+      // console.log(a, b);
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .a-swipe-item {
   min-height: 160px;
-  background: #364d79;
   color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  &.blue {
+    background: #364d79;
+  }
+  &.red {
+    background: red;
+  }
+  &.green {
+    background:green;
+  }
 }
 </style>
