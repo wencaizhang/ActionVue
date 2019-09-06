f5bde6790a19315d2f0278a6cddd62b056493141
wencaizhang <1052642137@qq.com>
2019-09-06 17:21:16 +0800

    docs: add plugin pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,13 +5,27 @@ module.exports = {
   description: 'ActionVue - vue 组件库',
   head: [
     ['link', { rel: 'icon', href: "/logo.png" }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: [
+    '@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: true
+    }
   ],
   extend: '@vuepress/theme-default',
   themeConfig: {
     nav: [
       {text: '主页', link: '/'},
       {text: '文档', link: '/docs/'},
-      {text: '开发笔记', link: '/notes/'},
+      // {text: '开发笔记', link: '/notes/'},
       // {text: 'GitHub', link: 'https://github.com/wencaizhang/ActionVue/'},
       // {text: '交流', link: 'https://github.com/wencaizhang/ActionVue/issues'},
     ],
