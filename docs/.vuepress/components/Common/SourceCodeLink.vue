099b4a0d33c11a9eb8b45517e93b1cba88628335
wencaizhang <1052642137@qq.com>
2019-12-05 17:25:06 +0800

    feat: create source code link for docs

---
+++ docs/.vuepress/components/Common/SourceCodeLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <GitHubLink :repo="url" />
+</template>
+
+<script>
+import GitHubLink from "./GitHubLink";
+export default {
+  name: 'SourceCodeLink',
+  components: { GitHubLink },
+  props: {
+    comp: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    url () {
+      return 'wencaizhang/ActionVue/tree/master/src/package/' + this.comp;
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
