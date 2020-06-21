13770fc3e8b88d7691787225108422292e9b4fad
wencaizhang <1052642137@qq.com>
2020-06-21 21:06:12 +0800

    delete useless file or code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
-    <router-view/>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  watch: {
-    '$route' (to, from) {
-      console.log(to, from);
-    }
-  }
 }
 </script>
 
