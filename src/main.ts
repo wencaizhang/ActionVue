43eeeee7b5900ed96383b12f05feb17a96e60850
wencaizhang <1052642137@qq.com>
2020-08-27 09:59:20 +0800

    引入vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { createWebHashHistory, createRouter } from "vue-router"
 
+import Hello from './components/Hello.vue'
+
+const history = createWebHashHistory()
+const router = createRouter({
+  history,
+  routes: [
+    { path: '/', component: Hello }
+  ]
+})
 createApp(App).mount('#app')
