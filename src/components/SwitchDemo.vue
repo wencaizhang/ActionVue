8b1a55a12537c18f814b68294e14704c2d454e83
wencaizhang <1052642137@qq.com>
2020-09-01 18:12:53 +0800

    styles: format code

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,9 +1,18 @@
 <template>
-<div>
-  switch demo
-</div>
+  <h1>Switch 示例</h1>
+  <DemoCode description="按钮是否禁用">
+    <Switch v-model:value="bool" />
+  </DemoCode>
 </template>
-
 <script lang="ts">
-
+import DemoCode from "../components/DemoCode.vue";
+import Switch from "../libs/Switch.vue";
+import { ref } from "vue";
+export default {
+  components: { Switch, DemoCode },
+  setup() {
+    const bool = ref(false);
+    return { bool };
+  },
+};
 </script>
---
+++ src/libs/Switch.vue
@@ -0,0 +1,63 @@
+<template>
+  <button class="a-switch" @click="toggle" :class="{'a-checked':value}">
+    <span></span>
+  </button>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
+  },
+};
+</script>
+
+<style lang="scss">
+$h: 22px;
+$h2: $h - 4px;
+.a-switch {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: $h/2;
+  position: relative;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+  &.a-checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.a-checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
+}
+</style>
